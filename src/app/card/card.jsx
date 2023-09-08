38afd2d9edfa7a93003ea0f780204393b5093527
Luan-Felipe <luanfel2009@gmail.com>
2023-09-08 16:07:12 -0300

    implementação do componente card

--- src/app/card/card.jsx
+++ src/app/card/card.jsx
@@ -1,6 +1,15 @@
-export default function Card(){
-    return (<>
-        <h3></h3>
-        <p></p>
-    </>)
+export default function Card({number, title, description}){
+    return (
+        <div className="w-96 max-sm:w-11/12 max-sm:mx-0 h-56 border border-gray-400 mx-2 text-left p-4 mb-2">
+                <div className="w-14 h-14 rounded-full bg-yellow-400 text-white font-noto font-bold justify-center text-4xl flex items-center mb-5">
+                  {number}
+                </div>
+                <h3 className="font-bold text-xl text-gray-800 mb-5">
+                  {title}
+                </h3>
+                <p className="font-thin">
+                  {description}
+                </p>
+        </div>
+    )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
+import Card from "./card/Card";
 
 export default function Home() {
   return (
     <>
       <main className="bg-white h-fit">
         <section>
-          <div className="w-screen h-72 bg-yellow-400 text-white text-center flex flex-col justify-center font-bold">
+          <div className="w-screen h-72 bg-yellow-400 text-white text-center flex flex-col justify-center font-semibold">
             <h1 className="text-4xl mb-5">TESTE VOCACIONAL GRÁTIS</h1>
             <p className="xl:text-xl sm:text-xs">
-              {" "}
               Descubra quais carreiras combinam com você com o nosso teste
               vocacional.
               <br />É grátis, bora lá?
@@ -23,47 +23,16 @@ export default function Home() {
         </section>
         <section>
           <div className="flex flex-col text-center">
-            <h2 className="font-bold text-4xl mt-20 max-sm:mt-5 text-gray-800">
+            <h2 className="font-semibold text-4xl mt-8 max-sm:mt-5 text-gray-800">
               Como funciona o teste vocacional?
             </h2>
-            <div className="flex flex-row flex-wrap mx-auto justify-center mt-16 max-sm:mt-5 mb-32 min-xl:w-5/6">
-              <div className="w-96 max-sm:w-11/12 max-sm:mx-0 h-56 border border-gray-800 mx-2 text-left p-4 mb-2">
-                <div className="w-14 h-14 rounded-full bg-yellow-400 text-white font-noto font-bold justify-center text-4xl flex items-center mb-5">
-                  1
-                </div>
-                <h3 className="font-bold text-xl text-gray-800 mb-5">
-                  Responda algumas perguntas
-                </h3>
-                <p className="font-thin">
-                  Vamos entender melhor o que você gosta <br />e não gosta para
-                  definir seu perfil.
-                </p>
-              </div>
-              <div className="w-96 max-sm:w-11/12 max-sm:mx-0 h-56 border border-gray-800 mx-2 text-left p-4 mb-2">
-                <div className="w-14 h-14 rounded-full bg-yellow-400 text-white font-noto font-bold justify-center text-4xl flex items-center mb-5">
-                  2
-                </div>
-                <h3 className="font-bold text-xl text-gray-800 mb-5">
-                  Descubra seu perfil
-                </h3>
-                <p className="font-thin">
-                  Depois de responder as perguntas, vamos gerar seu perfil e
-                  quais as principais carreiras para você.
-                </p>
-              </div>
-              <div className="w-96 max-sm:w-11/12 max-sm:mx-0 h-56 border border-gray-800 mx-2 text-left p-4 mb-2">
-                <div className="w-14 h-14 rounded-full bg-yellow-400 text-white font-noto font-bold justify-center text-4xl flex items-center mb-5">
-                  3
-                </div>
-                <h3 className="font-bold text-xl text-gray-800 mb-5">
-                  Comece a estudar
-                </h3>
-                <p className="font-thin">
-                  Agora que você descobriu sua carreira, que tal começar a
-                  estudar com bolsas de <br />
-                  até 75%?
-                </p>
-              </div>
+            <div className="flex flex-row flex-wrap mx-auto justify-center mt-5 max-sm:mt-5 mb-32 min-xl:w-5/6">
+              <Card number="1" title="Responda algumas perguntas" description="Vamos entender melhor o que você gosta e não gosta para
+                  definir seu perfil."/>
+              <Card number="2" title="Descubra seu perfil" description=" Depois de responder as perguntas, vamos gerar seu perfil e
+                  quais as principais carreiras para você."/>
+              <Card number="3" title="Comece a estudar" description="Agora que você descobriu sua carreira, que tal começar a
+                  estudar com bolsas de até 75%?"/>
             </div>
             <p className="font-semibold content-end text-gray-800">
               Está com dificuldades? É só clicar no botão abaixo e falar com a
